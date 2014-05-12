b1f35741f533b8034526c1e307bf4099fac0c5ac
chandrkant321@gmail.com <chandrkant321@gmail.com>
2014-05-12 10:42:16 +0530

    devisa app upload

---
+++ app/controllers/xyz_controller.rb
@@ -0,0 +1,10 @@
+class XyzController < ApplicationController
+  def index
+  end
+
+  def login
+  end
+
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookDevise::Application.config.secret_token = '0b125e50fe968b1a02f33a216a13e053f1e81b6ed327b72fb99f14f3145ce47dc20bce9d8a4a6751f59d308da3ef51970260fa31235c3423c5a05bde8e36e533'
