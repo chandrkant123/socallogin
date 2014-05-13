086719615ea321d4cebfdf78762ec33c3178a997
chandrkant123 <chandrkant321@gmail.com>
2014-05-13 12:02:33 +0530

    change in database yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140513052721) do
+ActiveRecord::Schema.define(:version => 20140513062819) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20140513052721) do
     t.string   "url"
     t.text     "description"
     t.text     "params"
+    t.text     "address"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
